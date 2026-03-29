66c4962807877c6a0d8e9d4874d3a56aa338c0ff
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-03-29 17:28:19 -0600

    test with a button

---
+++ src/lib/components/Wrapped/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { Button } from "PenguinMod-SvelteUI";
+
+    /**
+     * @type {import('svelte').ComponentProps<typeof Button>}
+     */
+    let props = $props();
+</script>
+
+<!-- TODO: load the theme from settings and automatically put it here -->
+<Button {...props} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
+    import Button from "$lib/components/Wrapped/Button.svelte";
 </script>
 
 <h1>Coming soon...</h1>
 <p>Dun dun...</p>
+<Button>Test</Button>
