c24114be6fc33ad2da0c19267285a2b8472385b4
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-06-10 04:52:23 -0600

    actually write that todo down

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+        // TODO: sveltekit PLEASE stop failing builds just because you couldnt find a url, missing urls literally dont matter to us at all
         adapter: adapter({
             // default options are shown. On some platforms
             // these options are set automatically — see below
