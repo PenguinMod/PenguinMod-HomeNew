5eba04fdd9d2a8b4f051c863bd9801101d0ef0e9
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-07-05 05:00:22 -0600

    I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.
     I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.  I dont care. I do not care.

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
 	plugins: [sveltekit()],
     optimizeDeps: {
         include: [
+            'highlight.js', 'lottie-web',
             'penguinmod',
         ],
         force: true
@@ -15,12 +16,18 @@ export default defineConfig({
         commonjsOptions: {
             include: [
                 /node_modules\/penguinmod/,
-            ]
+            ],
+            // This addresses the "default" export issue by allowing 
+            // these packages to be transformed into valid ESM
+            transformMixedEsModules: true,
+            include: [/node_modules/]
         },
     },
     ssr: {
+        // Force these packages to be processed as CJS
+        noExternal: ['highlight.js', 'lottie-web'],
         external: [
             'penguinmod'
         ],
-    }
+    },
 });
